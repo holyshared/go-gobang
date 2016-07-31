254a0ac0664971259099ba032b22c0d7329f0b3b
holyshared <holy.shared.design@gmail.com>
2016-07-31 16:29:57 +0900

    append GobangPlayer

--- gobang/npc_player.go
+++ gobang/npc_player.go
@@ -1,8 +1,7 @@
 package gobang
 
 type NpcPlayer struct {
-  game *GameContext
-  stone Stone
+  *GobangPlayer
 }
 
 func (npc *NpcPlayer) SelectTargetCell() *Cell {
@@ -10,32 +9,4 @@ func (npc *NpcPlayer) SelectTargetCell() *Cell {
     game: npc.game,
   }
   return ai.SelectTargetCell()
-}
-
-func (npc *NpcPlayer) PutStone(x, y int) (PutStoneResult, error) {
-  board := npc.game.CurrentBoard()
-
-  if !board.Have(x, y) {
-    return Failed, NewCellNotFoundError(NewPoint(x, y))
-  }
-
-  if !board.IsCellEmpty(x, y) {
-    return Failed, NewAlreadyPlacedError(NewPoint(x, y))
-  }
-
-  cell := board.Select(x, y)
-  npc.stone.PutTo(cell)
-
-  matcher := NewCellReachedMatcher(npc.stone, npc.game.ReachedStoneCount())
-  result := matcher.Matches(board)
-
-  if result.HasResult() {
-    return Reached, nil
-  }
-
-  if board.IsAllFilled() {
-    return Filled, nil
-  }
-
-  return Continue, nil
 }
--- gobang/player.go
+++ gobang/player.go
@@ -3,3 +3,36 @@ package gobang
 type Player interface {
   PutStone(x, y int) (PutStoneResult, error)
 }
+
+type GobangPlayer struct {
+  stone Stone
+  game *GameContext
+}
+
+func (player *GobangPlayer) PutStone(x, y int) (PutStoneResult, error) {
+  board := player.game.CurrentBoard()
+
+  if !board.Have(x, y) {
+    return Failed, NewCellNotFoundError(NewPoint(x, y))
+  }
+
+  if !board.IsCellEmpty(x, y) {
+    return Failed, NewAlreadyPlacedError(NewPoint(x, y))
+  }
+
+  cell := board.Select(x, y)
+  player.stone.PutTo(cell)
+
+  matcher := NewCellReachedMatcher(player.stone, player.game.ReachedStoneCount())
+  result := matcher.Matches(board)
+
+  if result.HasResult() {
+    return Reached, nil
+  }
+
+  if board.IsAllFilled() {
+    return Filled, nil
+  }
+
+  return Continue, nil
+}
