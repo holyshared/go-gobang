5c74e095beccb18141f53157a8d3d92a35e2be66
holyshared <holy.shared.design@gmail.com>
2016-08-16 15:36:03 +0900

    add gobang

--- gobang/gobang.go
+++ gobang/gobang.go
@@ -1,1 +1,18 @@
 package gobang
+
+func NewGobang() *Gobang {
+  context := NewGameContext(DefaultGameRule())
+  context.player = NewGamePlayer(Black, context)
+
+  ai := NewNpcArtificialIntelligence(context)
+  context.npcPlayer = NewNpcPlayer(White, ai, context)
+  context.currentPlayer = context.player
+
+  return &Gobang {
+    GameContext: context,
+  }
+}
+
+type Gobang struct {
+  *GameContext
+}
