554f722e2a940a716d911a2e4c19dbfaaacd8888
holyshared <holy.shared.design@gmail.com>
2016-08-20 17:45:09 +0900

    Remove gobang/game_facilitator.go

--- gobang/gobang.go
+++ gobang/gobang.go
@@ -4,21 +4,95 @@ import (
   "encoding/json"
 )
 
-func NewGobang(rule *GameRule, playerStone, npcPlayerStone Stone) *Gobang {
-  context := NewGameContext(rule, playerStone, npcPlayerStone)
-  facilitator := NewGameFacilitator(context)
+type GameFacilitator interface {
+  PlayerPutStoneTo(point *Point) (GameProgressResult, error)
+  NpcPlayerPutStone() (GameProgressResult, error)
+}
 
+func NewGobang(rule *GameRule, playerStone, npcPlayerStone Stone) *Gobang {
   return &Gobang {
-//    GameContext: context,
-    GameFacilitator: facilitator,
+    ctx: NewGameContext(rule, playerStone, npcPlayerStone),
   }
 }
 
 type Gobang struct {
-  //*GameContext
-  *GameFacilitator
+  ctx *GameContext
+}
+
+func (g *Gobang) PlayerPutStoneTo(point *Point) (GameProgressResult, error) {
+  cell, err := g.playerSelectCell(point)
+
+  if err != nil {
+    return Retry, err
+  }
+  result, err := g.playerPutStoneTo(cell)
+
+  if err != nil {
+    return Retry, err
+  }
+  g.changeToNextPlayer()
+
+  return result, err 
+}
+
+func (g *Gobang) NpcPlayerPutStone() (GameProgressResult, error) {
+  result, err := g.npcPlayerPutStone()
+
+  if err != nil {
+    return Retry, err //FIXME Fatal
+  }
+
+  return result, nil 
+}
+
+func (g *Gobang) playerSelectCell(point *Point) (*Cell, error) {
+  player := g.ctx.GamePlayer()
+  cell, err := player.SelectBoardCell(point)
+
+  return cell, err
+}
+
+func (g *Gobang) playerPutStoneTo(cell *Cell) (GameProgressResult, error) {
+  player := g.ctx.GamePlayer()
+  err := player.PutStoneTo(cell)
+
+  if err != nil {
+    return Retry, err
+  }
+  result := g.ctx.CheckBoard()
+
+  if result == Reached {
+    return Win, nil
+  } else if result == Filled {
+    return Draw, nil
+  }
+
+  return Next, nil
+}
+
+func (g *Gobang) npcPlayerPutStone() (GameProgressResult, error) {
+  player := g.ctx.NpcPlayer()
+  cell := player.SelectTargetCell()
+  err := player.PutStoneTo(cell)
+
+  if err != nil {
+    return Retry, err
+  }
+  result := g.ctx.CheckBoard()
+
+  if result == Reached {
+    return Lose, nil
+  } else if result == Filled {
+    return Draw, nil
+  }
+
+  return Next, nil
+}
+
+func (g *Gobang) changeToNextPlayer() {
+  g.ctx.ChangeToNextPlayer()
 }
 
 func (g *Gobang) MarshalJSON() ([]byte, error) {
-  return json.Marshal(g.GameFacilitator.ctx)
+  return json.Marshal(g.ctx)
 }
