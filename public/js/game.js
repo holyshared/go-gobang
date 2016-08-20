c1ecb87d7b96c6fec2e9e2b7fcccf7670f69caa3
holyshared <holy.shared.design@gmail.com>
2016-08-20 18:35:25 +0900

    add board style

--- public/js/game.js
+++ public/js/game.js
@@ -8,6 +8,8 @@
     this.websocket.onerror = this.onError.bind(this);
     this.output = output;
 
+    this.board = document.getElementById('board');
+
     this.startButton = document.getElementById('start');
     this.startButton.addEventListener('click', this.onStartClick.bind(this), false);
   }
@@ -27,6 +29,12 @@
   App.prototype.onMessage = function(evt) {
     console.log(evt)
     var msg = JSON.parse(evt.data)
+  
+    if (msg.type === 'start') {
+      this.board.setAttribute('class', 'display');
+      this.startButton.setAttribute('class', 'hidden');
+      renderBoard(this.board, msg.body.game);
+    }
   }
 
   App.prototype.onError = function(evt) {
@@ -39,6 +47,27 @@
       body: null
     });
     this.websocket.send(msg);
+  }
+
+  function renderBoard(el, game) {
+    var i = 0;
+    var x = 0;
+    var y = 0;
+
+    for (y = 0; game.board.size.height - 1 >= y; y++) {
+      for (x = 0; game.board.size.width - 1 >= x; x++) {
+        var cell = game.board.cells[i];
+        var c = document.createElement('li');
+
+        if (cell.stone === 1) {
+          c.innerText = 'B';
+        } else if (cell.stone === 2) {
+          c.innerText = 'W';
+        }
+        el.appendChild(c);
+        i++;
+      }
+    }
   }
 
   global.App = App;
