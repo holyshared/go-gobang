ab08b7cb8e482315872264e5ee9939e79f2d179c
holyshared <holy.shared.design@gmail.com>
2016-09-02 16:33:14 +0900

    add UnmarshalJSON

--- gobang/point_test.go
+++ gobang/point_test.go
@@ -20,3 +20,20 @@ func TestPointToJSON(t *testing.T) {
     t.Errorf("got %v\nwant %v", actual, expected)
   }
 }
+
+func TestJSONToPoint(t *testing.T) {
+  point := DefaultPoint()
+  err := json.Unmarshal([]byte("{\"x\":1,\"y\":1}"), &point)
+
+  if err != nil {
+    t.Errorf("error: %v", err)
+  }
+
+  if point.X() != 1 {
+    t.Errorf("got %v\nwant %v", point.X(), 1)
+  }
+
+  if point.Y() != 1 {
+    t.Errorf("got %v\nwant %v", point.Y(), 1)
+  }
+}
