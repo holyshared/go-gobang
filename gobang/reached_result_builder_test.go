35f6b0e52e9e6899bcea7de669496e28391b32ff
holyshared <holy.shared.design@gmail.com>
2016-09-02 15:37:21 +0900

    replace to NewCell function

--- gobang/reached_result_builder_test.go
+++ gobang/reached_result_builder_test.go
@@ -5,13 +5,14 @@ import (
 )
 
 func TestEmptyNeighborCells(t *testing.T) {
-  cells := make([]*Cell, 0)
-  neighborCells := make([]*Cell, 0)
-
-  cells = append(cells, &Cell { NewPoint(0, 0), Black, })
-  cells = append(cells, &Cell { NewPoint(1, 0), Black, })
-  cells = append(cells, &Cell { NewPoint(2, 0), Black, })
-  neighborCells = append(neighborCells, &Cell { NewPoint(3, 0), Black, })
+  cells := []*Cell {
+    NewCell(0, 0, Black),
+    NewCell(1, 0, Black),
+    NewCell(2, 0, Black),
+  }
+  neighborCells := []*Cell {
+    NewCell(3, 0, Black),
+  }
 
   builder := ReachedResultBuilder {
     cells: cells,
@@ -25,13 +26,14 @@ func TestEmptyNeighborCells(t *testing.T) {
     t.Errorf("got %v\nwant %v", len(emptyCells), 0)
   }
 
-  cells = cells[0:0]
-  neighborCells = neighborCells[0:0]
-
-  cells = append(cells, &Cell { NewPoint(0, 0), Black, })
-  cells = append(cells, &Cell { NewPoint(1, 0), Black, })
-  cells = append(cells, &Cell { NewPoint(2, 0), Black, })
-  neighborCells = append(neighborCells, &Cell { NewPoint(3, 0), 0, })
+  cells = []*Cell {
+    NewCell(0, 0, Black),
+    NewCell(1, 0, Black),
+    NewCell(2, 0, Black),
+  }
+  neighborCells = []*Cell {
+    NewCell(3, 0, 0),
+  }
 
   builder = ReachedResultBuilder {
     cells: cells,
